5139e963198a0473adf0d415ceb0d7ddbe9f55ae
pablocandela <pabloantonio@ciencias.unam.mx>
2016-11-03 22:28:12 -0600

    Progrma que imprime ¡Hola Mundo! en consola

---
+++ hola_mundo.c
@@ -0,0 +1,6 @@
+#include <stdio.h> //Se importa la bibloteca estandar de c.
+
+int main(){ //Es el metodo principal del programa.
+	printf("%s\n","¡Hola Mundo!"); //Se imprime la frase ¡Hola Mundo! con la funcion printf.
+	return 0; //Por convencion regresamos 0.
+}
